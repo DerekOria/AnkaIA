7ce794cfafefb06c5ecb76e34de7d0cf8236d132
DerekOria <derek_aldair@hotmail.com>
2026-05-26 14:00:05 -0400

    Correction des fichiers

--- ANKA/server.js
+++ ANKA/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { analyzeErrorLog } from './src/services/ollamaService.js';
+import { analyzeErrorLog } from './services/ollamaService.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
